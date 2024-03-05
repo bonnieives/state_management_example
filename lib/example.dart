42ac5a94a1b4ab1838f684e4787e2d8c17528739
Bonnie <bonnieives@gmail.com>
2024-03-05 15:54:33 -0500

    Initial commit

---
+++ lib/example.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/cupertino.dart';
+//
+// class MyText extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text('Needs data');
+//   }
+// }
+//
+// class MyTextField extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextField();
+//   }
+// }
